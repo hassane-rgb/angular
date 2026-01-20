466e8d4479d9c075ae171125e2c54bc81ed7c193
Hassane <moulayelhassane.sidiammi.ext@orange.com>
2026-01-20 15:11:35 +0100

    test(app): User resolver.

---
+++ src/app/users/user-detail.resolver.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { userDetailResolver } from './user-detail.resolver';
+import { UserStore } from './user.store';
+import { runInInjectionContext } from '@angular/core';
+import { vi } from 'vitest';
+
+describe('userDetailResolver', () => {
+  it('should select user from route param', () => {
+    TestBed.configureTestingModule({
+      providers: [UserStore],
+    });
+
+    const store = TestBed.inject(UserStore);
+    const spy = vi.spyOn(store, 'selectUser');
+
+    const route = {
+      paramMap: new Map([['id', '42']]),
+    } as unknown as ActivatedRouteSnapshot;
+
+    const state = {} as RouterStateSnapshot;
+
+    runInInjectionContext(TestBed.injector, () => {
+      userDetailResolver(route, state);
+    });
+
+    expect(spy).toHaveBeenCalledWith(42);
+  });
+});
